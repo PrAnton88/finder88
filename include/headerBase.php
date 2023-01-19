d161f5a725d4ef77b8e889db15e2c5902c70bdd4
Prudnikov Anton <prudnikovanton88mail.ru>
2023-01-19 09:47:28 +0300

    modify headerBase for saving text format in every restApi that included this file and pcodessing var dataRecord

--- include/headerBase.php
+++ include/headerBase.php
@@ -5,10 +5,26 @@
 	}
 	
 	if(!isset($_POST['dataRecord'])){
-		throw new ErrorException("Нет данных для записи");
+		throw new ErrorException("request data is invalid. dataRecord is not found");
 	}
 
 	$dataRecord = html_entity_decode(htmlspecialchars($_POST['dataRecord']));
+	
+	$dataRecord = str_replace('\'','/"',$dataRecord);
+	$dataRecord = str_replace('\\"','/"',$dataRecord);
+	$dataRecord = str_replace('\n','/n',$dataRecord);
+	$dataRecord = str_replace('\t','/tab',$dataRecord);
+	$dataRecord = str_replace('\\','/',$dataRecord);
+	$dataRecord = str_replace('/n','\n',$dataRecord);
+	$dataRecord = str_replace('/tab','\t',$dataRecord);
+	$dataRecord = str_replace('/"','\\"',$dataRecord);
+	
+	/* при вставке в html мы должны заменять tab на ряд пробелов */
+	// $dataRecord = str_replace('\t','&nbsp;&nbsp;&nbsp;',$dataRecord);
+	
+	$str = $dataRecord;
+	// echo $dataRecord;
+	
 	$dataRecord = json_decode($dataRecord,true);
 	
 ?>
