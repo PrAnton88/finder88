d25ed85b90d9a0d0ce05e72e31a2e45563f5a8e3
Prudnikov Anton <prudnikovanton88mail.ru>
2023-03-02 17:21:16 +0300

    modify queryGetComment for get ncontext and 'u.id as uid'

--- include/listapi/lib/query/get.query.Comments.php
+++ include/listapi/lib/query/get.query.Comments.php
@@ -1,7 +1,6 @@
 <?php
 
-	$queryGetComment = "Select c.*, UNIX_TIMESTAMP(c.date_reg) as date_reg, concat_ws(' ', u.last_name, u.first_name, u.patronymic) 'fio', i.image as avatar, i.path as avatarPath From `comments` as c  LEFT JOIN `users` as u ON u.role = c.user_link LEFT JOIN images_context as ic ON ic.ncontext = u.id 
-LEFT JOIN images as i ON i.id = ic.nimage ";
+	$queryGetComment = "Select c.*, ic.ncontext, UNIX_TIMESTAMP(c.date_reg) as date_reg, u.id as uid, concat_ws(' ', u.last_name, u.first_name, u.patronymic) 'fio', i.image as avatar, i.path as avatarPath From `comments` as c  LEFT JOIN `users` as u ON u.role = c.user_link LEFT JOIN images_context as ic ON ic.ncontext = u.id LEFT JOIN images as i ON i.id = ic.nimage ";
 
 	$queryGetChangeInfoComment = "Select id,request,user_link, UNIX_TIMESTAMP(date_reg) as date_reg, date_reg as date_reg_timestump, UNIX_TIMESTAMP(date_remove) as date_remove, date_remove as date_remove_timestump, hidden,parent From `comments` ";
 
