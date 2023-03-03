3b2e3e7cd3f2ee2a633cf900b505b37d80dc78af
Prudnikov Anton <prudnikovanton88mail.ru>
2023-03-03 13:23:18 +0300

    include/listapi/getDeptOfCompany.php: 1. transport query to preDataQuery; 2. enable use filters: ncomp, sdept, listcomps; 3. enable charset windows-1251 in response

--- include/listapi/getDeptOfCompany.php
+++ include/listapi/getDeptOfCompany.php
@@ -2,10 +2,17 @@
 header('Content-type:application/json;');
 
 	$getUserAuthData = true;
-	$sessAccesser = true;
+	$sessAccesser = false;
 	require_once "../start.php";
+	
+	require_once "../preDataQuery.php";
 
-if($resolution){
+try{
+	
+	if(!$resolution){
+		header("HTTP/1.1 403 Forbidden"); exit;
+	}
+	
 	
 	$comp = false;
 	if(isset($_POST["comp"])){
@@ -13,30 +20,114 @@ if($resolution){
 		if($comp <= 0){ $comp = false; }
 	}
 	
+	$query = $deptsPreQuery." WHERE p.hidden=0 AND d.hidden=0 ";
 	
-	if($comp){
-		$query = "SELECT d.id, d.name FROM dept as d LEFT JOIN place1 as p ON p.dept = d.id ";
-		
-		$query .= " WHERE p.comp='$comp' ";
-		
-		$query .= "AND p.hidden=0 AND d.hidden=0 GROUP BY p.dept";
-		
-	}else{
-		$query = "SELECT d.id, d.name FROM dept as d ";
-		$query .= "WHERE d.hidden=0 GROUP BY d.name";
-		
-		
+	$dataRecord = false;
+	if(isset($_POST['dataRecord'])){
+		$dataRecord = html_entity_decode(htmlspecialchars($_POST['dataRecord']));
+		$dataRecord = json_decode($dataRecord,true);
 	}
 	
 	
-	$result = $db->fetchAssoc($query,$uid);
+	$selectAnyDept = false;
+	$tmp = false;
+	$isEmptyData = false;
+	$ncomp = false;
 	
-	if((is_string($result)) && (strpos($result,"error") !== false)){
-		header("HTTP/1.1 500 SQL Error: $result");
+	if($comp !== false){
+		$query .= " AND p.comp=".$comp;
 	}else{
-		echo toJson($result,true/*Assoc*/);
+		
+		$forAppend = false;
+		if(isset($dataRecord["listcomps"])){
+			
+			$listcomps = $dataRecord["listcomps"];
+			foreach($listcomps as $item){
+				$tmp = (int) $item;
+				if($tmp > 0){
+					
+					if(!$forAppend){
+						$query .= " AND ";
+					}else{
+						$query .= " OR ";
+					}
+					
+					$query .= "p.comp=".$tmp;
+					$selectAnyDept = true;
+					
+					$forAppend = true;
+				}
+				
+			}
+		}
+		
+		if((!$forAppend) && isset($dataRecord["ncomp"])){
+			$tmp = (int) $dataRecord["ncomp"];
+			if($tmp > 0){
+				$query .= " AND p.comp=".$tmp;
+				$selectAnyDept = true;
+				$ncomp = $tmp;
+				
+			}elseif($tmp < 0){
+				$isEmptyData = true;
+			}
+		}
+		
+		if(isset($dataRecord["sdept"])){
+			$tmp = $dataRecord["sdept"];
+			$tmp = mb_strtolower(trim($tmp));
+			if($tmp != ''){
+				$query .= " AND (LOWER(d.name) LIKE '%".$tmp."%') ";
+				$selectAnyDept = true;
+			}
+		}
+	}
+	if(($tmp === 0) || ($tmp == '')){ 
+		$selectAnyDept = false;
 	}
 	
+	if($isEmptyData){
+		// $result = array('id'=>'-1','deptname'=>'Нет данных');
+		$result = array(array('id'=>'0','deptname'=>'Все отделы'));
+	}else{
 
-}else{ header("HTTP/1.1 403 Forbidden"); }
+		$query .= " GROUP BY d.name ORDER BY d.name ASC";
+		// echo $query;
+		
+		$result = $db->fetchAssoc($query,$uid);
+		if((is_string($result)) && (strpos($result,"error") !== false)){
+			throw new ErrorException("SQL Error"); 
+		}
+		
+	/* if($selectAnyDept === false){
+		array_unshift($result,array('id'=>'0','deptname'=>'Все отделы'));
+	} */
+		
+		
+		if(count($result) > 0){
+			array_unshift($result,array('id'=>'0','deptname'=>'Все отделы'.((($ncomp !== false) && ($ncomp > 0))?' организации':'')));
+		}else{
+			// array_unshift($result,array('id'=>'-1','deptname'=>'Нет данных'));
+			$result = array(array('id'=>'-1','deptname'=>'Нет данных'));
+		}
+		
+	}
+		
+	echo '{"success":1,"description":"issetData","listData":'.json_encode($result).'}';
+
+}catch(ErrorException $ex){
+	/* - когда html
+	echo '<script>new UserException("'.exc_handler($ex).'").log();</script>';
+	*/
+	$ex = exc_handler($ex);
+	$ex = iconv("utf-8","windows-1251//IGNORE",$ex);
+	
+	echo '{"success":0,"description":"'.$ex.'"}';
+	
+}catch(Exception $ex){
+	
+	$ex = exc_handler($ex);
+	$ex = iconv("utf-8","windows-1251//IGNORE",$ex);
+	echo '{"success":0,"description":"'.$ex.'"}';
+}
 ?>
